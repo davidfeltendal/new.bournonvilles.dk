2ef13ec640526241b73b301de60e72ddd44c8212
massidk <e-david@live.dk>
2016-05-29 16:04:34 +0200

    pages addet in umbraco

---
+++ new.bournonvilles.dk/Scripts/navBar.js
@@ -0,0 +1,8 @@
+﻿//$(document).ready(function () {
+//    $('.dropdown-submenu>a').unbind('mouseover').mouseover(function (e) {
+//        $(this).next('ul').toggle();
+//        e.stopPropagation();
+//        e.preventDefault();
+//    });
+//});
+
